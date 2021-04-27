9fe79a21127c84e02d033d5a45e546dff8094c39
sduddella <duddellasiri@gmail.com>
2021-04-26 23:03:14 -0400

    Finished Board.java stubs

--- Board.java
+++ Board.java
@@ -21,13 +21,13 @@ public class Board extends JPanel {
    /** creates a serialized object to keep track of users */
    private static final long serialVersionUID = 1L;
    /** creates an object of PacMan named "pacman" */
-   //private PacMan pacman;
+   private PacMan pacman;
    /** instantiates Driver object game */
    Driver game;
    /** array of ghosts */
    private GhostDriver[] ghosts;
-   /** instantiates background and items panels*/
-   private JPanel background, itemBoard;
+   /** instantiates background and food panels*/
+   private JPanel background, foodBoard;
    /** instantiates multiboard Layered Pane*/
    private JLayeredPane multiboard;
    /** integers that track map height and width **/
@@ -58,5 +58,74 @@ public class Board extends JPanel {
    *  creates Driver object called thisGame
    */
    public Board(Driver thisGame) throws IOException{}
+   
+   /** This method creates a status bar for the game */
+   private void MakeStatusBar(){}
+   
+   /** This method draws food */
+   private void CreateFood(){}
+   
+   /** This method draws the background of the maze 
+   * @return JPanel = background 
+   */
+   private JPanel drawBackground(){}
+   
+   /** Function that returns a new pacman in position
+   * @return Pacman = initial position
+   */
+   public PacMan position(){}
 
+   /** function that ghosts ghosts in position
+   * @param pos
+   *  provides expected location of Ghosts
+   * @return Ghost = inital Ghost position 
+   */
+   public GhostDriver ghostPos(int pos){}
+   
+   /** Constructor - map width */
+   public int getMapWidth(){}
+   
+   /** Constructor - map height */
+   public int getMapHeight(){}
+   
+   /** Constructor - toString */
+   public String toString(){}
+   
+   /** function that adds food to main board 
+   * @param comp
+   *   component
+   * @param num
+   *   position
+   */
+   public void addFoodToMultiBoard(Component comp, Integer num){}
+   
+   /** check if position empty
+   * @param i
+   *  first index on board position
+   * @param j
+   *  second index on board position
+   */
+   public boolean isClear(int i, int j){}
+   
+   /** method to eat food
+   * @param i
+   *  first index on board position
+   * @param j
+   *  second index on board position
+   */
+   public void eat(int i, int j){}
+   
+   /** method describes winning */
+   private void winGameEvent(){}
+   
+   /** method restarts game */
+   private void  restart(){}
+   
+   /** method to end game */
+   private void endGame(){}
+   
+   /** method to stop characters */
+   private void stopCharacters(){}
+   
+   
 }
